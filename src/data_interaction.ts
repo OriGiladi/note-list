53f8ec5df661a0415036ae0969d7716de5c58e75
origiladi <origiladi8@gmail.com>
2023-08-24 22:23:27 +0300

    data interaction correction (exept error handling)

--- src/data_interaction.ts
+++ src/data_interaction.ts
@@ -7,37 +7,46 @@ export class DataInteraction {
     this.title = title;
   }
 
-  addNote(note: note_obj): boolean {
+  LocalHostGet(): string | null{   // Get a note from localStorage based on the current title
+    return localStorage.getItem(this.title)
+  }
+  LocalHostSet(note: note_obj): void{ // Store a note in localStorage using the current title
+    localStorage.setItem(this.title, JSON.stringify(note))
+  }
+  LocalHostDelete(): void{ // Delete a note from localStorage using the current title
+    localStorage.removeItem(this.title);
+  }
+
+  addNote(note: note_obj): boolean { // Add a new note if body is not empty and note with the current title doesn't exist
     if (note.body === "") {
-      console.log("there's no note body");
       return false;
-    } else if (localStorage.getItem(this.title) !== null) {
+    } else if (this.LocalHostGet() !== null) {
       return false;
     } else {
-      localStorage.setItem(this.title, JSON.stringify(note));
+      this.LocalHostSet(note);
       return true;
     }
   }
 
-  getTitle(): string {
+  getTitle(): string {  // Get the current title
     return this.title;
   }
-  search(): string {
-    const noteBody = localStorage.getItem(this.title);
+  search(): string {  // Search for and retrieve the body of the note with the current title
+    const noteBody = this.LocalHostGet();
     if (noteBody !== null) return noteBody;
     else return "";
   }
-  delete(): string {
-    const noteBody = localStorage.getItem(this.title);
+  delete(): string { // Delete the note with the current title if it exist
+    const noteBody = this.LocalHostGet();
     if (noteBody !== null) {
-      localStorage.removeItem(this.title);
+      this.LocalHostDelete();
       return ``;
     } else return `There's no note named ${this.title}`;
   }
-  findSimilar(note_list: Note[], input: string): Note[] {
+  findSimilar(note_list: Note[], input: string): Note[] { //finds notes that consists of the input given 
     let result: Note[] = [];
     note_list.forEach((note) => {
-      const regex = new RegExp(input, "g");
+      const regex = new RegExp(input);
       if (note.title.match(regex)) result.push(note);
     });
     return result;
